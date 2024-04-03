c59538b1b68d13d681c97258010584deab273986
Hardi Rachh <shahhardi71@gmail.com>
2024-04-03 18:42:50 +0100

    Changed folder and file name accordingly UI.

---
+++ app/src/main/java/com/hardi/newsapp/ui/common/ArticleLayout.kt
@@ -0,0 +1,90 @@
+package com.hardi.newsapp.ui.common
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.hardi.newsapp.data.model.Article
+import com.hardi.newsapp.data.model.Source
+
+
+@Composable
+fun ArticleUI(article: Article, onNewsClick: (url: String) -> Unit) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable {
+                if (article.url.isNotEmpty()) {
+                    onNewsClick(article.url)
+                }
+            }) {
+        BannerImage(article)
+        TitleText(article.title)
+        DescriptionText(article.description)
+        SourceText(article.source)
+    }
+}
+
+@Composable
+fun SourceText(source: Source) {
+    Text(
+        text = source.name,
+        style = MaterialTheme.typography.titleSmall,
+        color = Color.Gray,
+        maxLines = 2,
+        modifier = Modifier.padding(
+            start = 5.dp,
+            end = 5.dp,
+            top = 5.dp,
+            bottom = 5.dp
+        )
+    )
+}
+
+@Composable
+fun DescriptionText(description: String?) {
+    if (!description.isNullOrEmpty()) {
+        Text(
+            text = description,
+            style = MaterialTheme.typography.bodyMedium,
+            color = Color.Gray,
+            maxLines = 2,
+            modifier = Modifier.padding(5.dp)
+        )
+    }
+}
+
+@Composable
+fun TitleText(title: String) {
+    if (title.isNotEmpty()) {
+        Text(
+            text = title,
+            style = MaterialTheme.typography.titleMedium,
+            color = Color.Black,
+            maxLines = 2,
+            modifier = Modifier.padding(5.dp)
+        )
+    }
+}
+
+@Composable
+fun BannerImage(article: Article) {
+    AsyncImage(
+        model = article.imageUrl,
+        contentDescription = article.title,
+        contentScale = ContentScale.Crop,
+        modifier = Modifier
+            .height(200.dp)
+            .fillMaxWidth()
+    )
+}
+
